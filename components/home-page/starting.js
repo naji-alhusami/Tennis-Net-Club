d42814e3637ec58b5bce923d504a037676fcb34c
naji-alhusami <najihussami@gmail.com>
2023-08-24 15:55:35 +0300

    finished navbar

--- components/home-page/starting.js
+++ components/home-page/starting.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Image from "next/image";
 
+import MainNavigation from "../layout/main-navigation";
 import background from "@/public/images/background.jpg";
 import classes from "./starting.module.css";
 
 function starting() {
   return (
-    <section className={classes.starting}>
-      <div className={classes.image}>
-        <Image src={background} alt="website background" />
-      </div>
-      <div className={classes.startingText}>
-        <h1>Hi</h1>
-        <h2>Welcome to the heart of tennis history and inspiration!</h2>
-        <p>Tennis Legends Series a captivating journey into the lives, triumphs, and lasting legacies of the remarkable individuals who have graced the courts and etched their names into tennis lore.</p>
-      </div>
-    </section>
+    <Fragment>
+      <section className={classes.starting}>
+        <MainNavigation />
+        <div className={classes.image}>
+          <Image src={background} alt="website background" />
+        </div>
+        <div className={classes.startingText}>
+          <h1>Hi</h1>
+          <h2>Welcome to the heart of tennis history and inspiration!</h2>
+          <p>
+            Tennis Legends Series a captivating journey into the lives,
+            triumphs, and lasting legacies of the remarkable individuals who
+            have graced the courts and etched their names into tennis lore.
+          </p>
+        </div>
+      </section>
+    </Fragment>
   );
 }
 
---
+++ components/layout/logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./logo.module.css";
+
+function Logo() {
+  return <div className={classes.logo}>TENNIS BLOGS</div>;
+}
+
+export default Logo;
