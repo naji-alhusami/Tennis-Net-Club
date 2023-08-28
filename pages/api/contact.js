a1535f77738ab6899e7844a928b5bd58f0fe13b5
naji-alhusami <najihussami@gmail.com>
2023-08-28 18:49:52 +0300

    finished mongoDB configuration

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body; //we bring them from body: JSON.stringify
 
@@ -21,7 +23,28 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://najihussami:wSDSJON1BVihGY6H@cluster0.p5janj2.mongodb.net/mySite?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could connect to Database." });
+      return;
+    }
+
+    const db = client.db();
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed." });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
