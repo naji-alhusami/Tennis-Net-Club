59311b5829a6f595099926ff02628ded4e6f7cfb
naji-alhusami <najihussami@gmail.com>
2023-09-06 16:10:26 +0300

    finished the functions of date picker

--- components/home-page/booking.js
+++ components/home-page/booking.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
 
 import Image from "next/image";
 
 import classes from "./booking.module.css";
 import bookCourt from "@/public/images/bookCourt.jpg";
 import bg from "@/public/images/background1.jpg";
+import BookingDate from "./bookingDate";
 
 function Booking() {
+    const [numberOfPlayers, setNumberOfPlayers] = useState(0);
+  
+
+  const decreasePlayers = () => {
+    if (numberOfPlayers > 1) {
+      setNumberOfPlayers(numberOfPlayers - 1);
+    }
+  };
+
+  const increasePlayers = () => {
+    if (numberOfPlayers < 4) {
+      setNumberOfPlayers(numberOfPlayers + 1);
+    }
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.imageContainer}>
@@ -14,12 +33,43 @@ function Booking() {
           <Image src={bg} alt="cover-image" layout="fill" objectFit="cover" />
         </div>
         <div className={classes.textContainer}>
-          <h4>Best Courts</h4>
-          <h1>Reserve Your Court</h1>
-          <p>
-            Come and Reserve your courtCome and Reserve your courtCome and
-            Reserve your courtCome and Reserve your court
-          </p>
+          <div className={classes.text}>
+            <h4>Best Courts</h4>
+            <h1>Reserve Your Court</h1>
+            <p>
+              Come and Reserve your courtCome and Reserve your courtCome and
+              Reserve your courtCome and Reserve your court
+            </p>
+          </div>
+          <div className={classes.bookingContainer}>
+            <BookingDate />
+            {/* <div className={classes.dateContainer}>
+              <h3>Date:</h3>
+              <DatePicker
+                selected={selectedDate}
+                value={selectedDate}
+                onChange={handleDateChange}
+                placeholderText="Select a date"
+              />
+              <button
+                className={classes.calendarButton}
+                onClick={toggleCalendar}
+              >
+                A
+              </button>
+            </div> */}
+            <div className={classes.playersContainer}>
+              <h3>Players:</h3>
+              <button onClick={decreasePlayers}>-</button>
+              <span>{numberOfPlayers}</span>
+              <button onClick={increasePlayers}>+</button>
+            </div>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <Link className={classes.reserveButton} href="/auth/login">
+                Reserve Court
+              </Link>
+            </motion.div>
+          </div>
         </div>
       </div>
       <div className={classes.imageContainer}>
---
+++ components/home-page/bookingDate.js
@@ -0,0 +1,57 @@
+import React, { useState, useRef } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import classes from "./booking.module.css";
+
+function BookingDate() {
+  const [calendarIsOpen, setCalendarIsOpen] = useState(false);
+  const formattedDate = new Date().toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const [selectedDate, setSelectedDate] = useState(formattedDate);
+  const datePickerRef = useRef(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(
+      date.toLocaleDateString("en-US", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      })
+    );
+    setCalendarIsOpen(false);
+  };
+
+  const toggleCalendar = () => {
+    if (datePickerRef.current) {
+      datePickerRef.current.setOpen(true);
+    }
+  };
+
+  return (
+    <div className={classes.dateContainer}>
+      <h3>Date:</h3>
+      <div className={classes.dateDisplay}>
+        {selectedDate}
+        <button onClick={toggleCalendar}>Change</button>
+        <div className={classes.reactDatePicker}>
+          <DatePicker
+            selected={new Date(selectedDate)}
+            onChange={handleDateChange}
+            placeholderText=""
+            ref={datePickerRef}
+            calendarClassName={
+              calendarIsOpen ? "show-calendar" : "hide-calendar"
+            }
+            showPopperArrow={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookingDate;
