7c1de58288ed49cc76d6ccab5bd79a18b81ca6ca
naji-alhusami <najihussami@gmail.com>
2023-09-07 14:51:53 +0300

    added pictures of playerForm and dateForm

--- components/booking/booking-court.js
+++ components/booking/booking-court.js
@@ -2,15 +2,43 @@ import React from "react";
 
 import Image from "next/image";
 
-import reserve from "@/public/images/reserve.jpg";
 import classes from "./booking-court.module.css";
+import reserve from "@/public/images/reserve.jpg";
+import clay from "@/public/images/clay.jpg";
+import calendarBackground from "@/public/images/calendarBackground.jpg";
 
 function BookingCourt() {
   return (
-    <div>
+    <div className={classes.bookingContainer}>
       <div className={classes.imageContainer}>
-        <Image src={reserve} alt="reserve-court" />
+        <Image
+          src={reserve}
+          alt="reserve-court"
+          style={{ filter: "brightness(0.8)" }}
+        />
         <h1>Reserve Court</h1>
+      </div>
+      <div className={classes.stepsContainer}>
+        <div className={classes.stepContainer}>
+          <div className={classes.stepsCircle}>1</div>
+          <h3>SELECT</h3>
+        </div>
+        <div className={classes.stepContainer}>
+          <div className={classes.stepsCircle}>2</div>
+          <h3>DETAILS</h3>
+        </div>
+        <div className={classes.stepContainer}>
+          <div className={classes.stepsCircle}>3</div>
+          <h3>CONFIRM</h3>
+        </div>
+      </div>
+      <div className={classes.bookingForm}>
+        <div className={classes.bookingPlayers}>
+          <Image src={clay} alt="clay-courts" />
+        </div>
+        <div className={classes.bookingDate}>
+          <Image src={calendarBackground} alt="calender-bg" />
+        </div>
       </div>
     </div>
   );
