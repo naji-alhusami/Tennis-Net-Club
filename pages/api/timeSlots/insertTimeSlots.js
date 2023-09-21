74f5d941e5be769f171eebbf88634104518eae84
naji-alhusami <najihussami@gmail.com>
2023-09-21 14:08:36 +0300

    added times to Mongo and replace them with the old ones

--- pages/api/timeSlots/insertTimeSlots.js
+++ pages/api/timeSlots/insertTimeSlots.js
@@ -1,37 +1,3 @@
-// import { connectToDatabase } from "@/lib/db";
-
-// async function insertTimesSlotsHandler(req, res) {
-//   if (req.method === "POST") {
-//     console.log(req.body);
-
-//     const client = await connectToDatabase();
-
-//     const timeSlots = req.body.map((slot) => ({
-//       date: slot.date,
-//       time: slot.time,
-//       status: slot.status,
-//     }));
-
-//     const db = client.db();
-//     try {
-//       const result = await db.collection("times").insertMany(timeSlots);
-//       newTimes.id = result.insertedIds;
-//     } catch (error) {
-//       client.close();
-//       res.status(500).json({ message: "Storing message failed." });
-//       return;
-//     }
-
-//     client.close();
-
-//     res
-//       .status(201)
-//       .json({ message: "Successfully stored message!", times: timeSlots });
-//   }
-// }
-
-// export default insertTimesSlotsHandler;
-
 import { connectToDatabase } from "@/lib/db";
 
 async function insertTimesSlotsHandler(req, res) {
@@ -39,17 +5,26 @@ async function insertTimesSlotsHandler(req, res) {
     console.log(req.body);
 
     const client = await connectToDatabase();
-
-    // Convert date and time strings to JavaScript Date objects
-    const timeSlots = req.body.map((slot) => ({
-      date: slot.date,
-      time: slot.time,
-      status: slot.status,
-    }));
-
     const db = client.db();
+
+    let timeSlots;
+
     try {
+      const existingTimes = await db.collection("times").countDocuments();
+      console.log(existingTimes);
+
+      if (existingTimes > 0) {
+        await db.collection("times").deleteMany({});
+      }
+      // Convert date and time strings to JavaScript Date objects
+      const timeSlots = req.body.map((slot) => ({
+        date: slot.date,
+        time: slot.time,
+        status: slot.status,
+      }));
+
       const result = await db.collection("times").insertMany(timeSlots);
+      timeSlots.id = result.insertedIds;
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Storing message failed." });
