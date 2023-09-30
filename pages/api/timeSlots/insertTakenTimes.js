de7d554585a2fb11485445b57c5b52f06b68a174
naji-alhusami <najihussami@gmail.com>
2023-09-30 15:54:09 +0300

    added expiration date for each take time

--- pages/api/timeSlots/insertTakenTimes.js
+++ pages/api/timeSlots/insertTakenTimes.js
@@ -6,15 +6,21 @@ async function insertTakenTimesHandler(req, res) {
     const client = await connectToDatabase();
     const db = client.db();
 
+    // const takenTimeDate = new Date(date);
+    // const expirationDate = new Date(takenTimeDate);
+    // expirationDate.setDate(takenTimeDate.getMinutes() + 1);
+    const currentDate = new Date();
+    const expirationDate = new Date(currentDate);
+    expirationDate.setHours(currentDate.getHours() + 4);
+    console.log(expirationDate);
+
     const takenTime = {
       _id: _id,
       time: time,
       date: date,
       status: false,
+      expireAt: expirationDate,
     };
-
-    console.log(takenTime);
-    // let takenTimes;
 
     try {
       const result = await db.collection("takenTimes").insertOne(takenTime);
@@ -23,9 +29,10 @@ async function insertTakenTimesHandler(req, res) {
     } finally {
       client.close();
     }
-    res
-      .status(201)
-      .json({ message: "Successfully stored taken times!", takenTimes: takenTime });
+    res.status(201).json({
+      message: "Successfully stored taken times!",
+      takenTimes: takenTime,
+    });
   }
 }
 
