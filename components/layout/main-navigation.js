c840d5b93a0c488191d1f576be819a09bfa809f7
naji-alhusami <najihussami@gmail.com>
2023-10-03 12:11:23 +0300

    working on main navigation styling

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { AiFillCaretDown } from "react-icons/ai";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import Logo from "./logo";
 
 import classes from "./main-navigation.module.css";
-import Image from "next/image";
-import navbar from "../../components/icons/navbar.svg";
-import cancelNavbar from "../../components/icons/cancelNavbar.svg";
 
 function MainNavigation(props) {
   const [isToggled, setIsToggled] = useState(false);
+  const [showSideNavbar, setShowSideNavbar] = useState(false);
   const { backgroundColor } = props;
   const { data: session, loading } = useSession();
 
@@ -22,7 +20,7 @@ function MainNavigation(props) {
 
   function toggleButtonHandler() {
     setIsToggled(!isToggled);
-    console.log("clicked");
+    setShowSideNavbar(!showSideNavbar);
   }
 
   const headerStyle = {
@@ -30,129 +28,152 @@ function MainNavigation(props) {
   };
 
   return (
-    <header className={classes.header} style={headerStyle}>
-      <Link href="/">
-        <Logo />
-      </Link>
-      <nav>
-        <ul className={classes.ul}>
-          {/* Large Screens */}
-          <li>
-            <Link className={classes.navbarListHome} href="/">
-              Home
-            </Link>
-          </li>
-          {session && (
+    <div className={classes.container}>
+      <header className={classes.header} style={headerStyle}>
+        <Link href="/">
+          <Logo />
+        </Link>
+        <nav>
+          <ul className={classes.ul}>
+            {/* Large Screens */}
             <li>
-              <Link className={classes.navbarListBook} href="/">
-                Book Court
-              </Link>
-            </li>
-          )}
-          {session && (
-            <li>
-              <Link className={classes.navbarListPartner} href="/">
-                Find Partner
-              </Link>
-            </li>
-          )}
-          {session && (
-            <li>
-              <Link className={classes.navbarListTraining} href="/">
-                Trainings
-              </Link>
-            </li>
-          )}
-          <li>
-            <Link className={classes.navbarListBlogs} href="/posts">
-              Blogs
-            </Link>
-          </li>
-          <li>
-            <Link className={classes.navbarListContact} href="/contact">
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link className={classes.navbarListAbout} href="/about">
-              About
-            </Link>
-          </li>
-
-          {/* Med Screens */}
-          {!session && (
-            <li>
-              <Link className={classes.navbarListHomeMed} href="/">
+              <Link className={classes.navbarListHome} href="/">
                 Home
               </Link>
             </li>
-          )}
-          {!session && (
+            {session && (
+              <li>
+                <Link className={classes.navbarListBook} href="/">
+                  Book Court
+                </Link>
+              </li>
+            )}
+            {session && (
+              <li>
+                <Link className={classes.navbarListPartner} href="/">
+                  Find Partner
+                </Link>
+              </li>
+            )}
+            {session && (
+              <li>
+                <Link className={classes.navbarListTraining} href="/">
+                  Trainings
+                </Link>
+              </li>
+            )}
             <li>
-              <Link className={classes.navbarListBlogsMed} href="/">
+              <Link className={classes.navbarListBlogs} href="/posts">
                 Blogs
               </Link>
             </li>
-          )}
-          {!session && (
             <li>
-              <Link className={classes.navbarListContactMed} href="/">
+              <Link className={classes.navbarListContact} href="/contact">
                 Contact
               </Link>
             </li>
-          )}
-          {!session && (
             <li>
-              <Link className={classes.navbarListAboutMed} href="/">
+              <Link className={classes.navbarListAbout} href="/about">
                 About
               </Link>
             </li>
-          )}
-          <li>
-            {!session && !loading && (
-              <motion.div
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                className={classes.login}
-              >
-                <Link className={classes.loginButton} href="/auth/login">
-                  Login
+
+            {/* Med Screens */}
+            {!session && (
+              <li>
+                <Link className={classes.navbarListHomeMed} href="/">
+                  Home
                 </Link>
-              </motion.div>
+              </li>
             )}
-          </li>
-          <li>
-            {session && (
-              <motion.div whileHover={{ scale: 1.1 }} className={classes.user}>
-                <div className={classes.userName}>
-                  {session?.user.name}{" "}
-                  <AiFillCaretDown className={classes.icon} />
-                  <Link
-                    className={classes.list}
-                    href="/"
-                    onClick={logoutHandler}
-                  >
-                    <div className={classes.userList}>Logout</div>
+            {!session && (
+              <li>
+                <Link className={classes.navbarListBlogsMed} href="/">
+                  Blogs
+                </Link>
+              </li>
+            )}
+            {!session && (
+              <li>
+                <Link className={classes.navbarListContactMed} href="/">
+                  Contact
+                </Link>
+              </li>
+            )}
+            {!session && (
+              <li>
+                <Link className={classes.navbarListAboutMed} href="/">
+                  About
+                </Link>
+              </li>
+            )}
+            <li>
+              {!session && !loading && (
+                <motion.div
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  className={classes.login}
+                >
+                  <Link className={classes.loginButton} href="/auth/login">
+                    Login
                   </Link>
-                </div>
-              </motion.div>
-            )}
-          </li>
-          <li>
-            <div
-              className={`${classes.navbarToggleContainer} ${
-                isToggled ? `${classes.change}` : ""
-              }`}
-              onClick={toggleButtonHandler}
-            >
-              <div className={classes.line1}></div>
-              <div className={classes.line2}></div>
-              <div className={classes.line3}></div>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </header>
+                </motion.div>
+              )}
+            </li>
+            <li>
+              {session && (
+                <motion.div
+                  whileHover={{ scale: 1.1 }}
+                  className={classes.user}
+                >
+                  <div className={classes.userName}>
+                    {session?.user.name}{" "}
+                    <AiFillCaretDown className={classes.icon} />
+                    <Link
+                      className={classes.list}
+                      href="/"
+                      onClick={logoutHandler}
+                    >
+                      <div className={classes.userList}>Logout</div>
+                    </Link>
+                  </div>
+                </motion.div>
+              )}
+            </li>
+            <li>
+              <div
+                className={`${classes.navbarToggleContainer} ${
+                  isToggled ? `${classes.change}` : ""
+                }`}
+                onClick={toggleButtonHandler}
+              >
+                <div className={classes.line1}></div>
+                <div className={classes.line2}></div>
+                <div className={classes.line3}></div>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <AnimatePresence>
+        {showSideNavbar && (
+          <motion.div
+            className={`${classes.sideNavbarContainer} ${
+              showSideNavbar ? `${classes.show}` : ""
+            }`}
+            transition={{ duration: 0.5 }}
+            initial={{ x: showSideNavbar ? "100%" : "0%" }}
+            animate={{ x: showSideNavbar ? "0%" : "100%" }}
+            exit={{ x: "100%" }}
+          >
+            <p>home</p>
+            <p>home</p>
+            <p>home</p>
+            <p>home</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
 
---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import TestForm from "@/components/contact/test-form";
+
+function TestPage() {
+  return (
+    <Fragment>
+      <TestForm />
+    </Fragment>
+  );
+}
+
+export default TestPage;
