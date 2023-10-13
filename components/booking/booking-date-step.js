d03f5c051e3903ae9e91fa55f7560878591796d1
naji-alhusami <najihussami@gmail.com>
2023-10-13 20:37:40 +0300

    working on styling the first step in booking page

--- components/booking/booking-date-step.js
+++ components/booking/booking-date-step.js
@@ -12,24 +12,13 @@ import AuthContext from "@/store/auth-context";
 
 import classes from "./booking-date-step.js.module.css";
 import Link from "next/link";
+import { RightArrow } from "../icons/right-arrow";
 
-function DateSelectionStep({
-  handleChangeCourts,
-  // selectedCourtType,
-  isShowCourts,
-  setIsShowCourts,
-}) {
+function DateSelectionStep() {
   const searchParams = useSearchParams();
   const path = searchParams.has("date");
 
-  const {
-    activeDay,
-    numberOfPlayers,
-    setNumberOfPlayers,
-    nextButton,
-    nextStepHandler,
-    currentStep,
-  } = useContext(AuthContext);
+  const { activeDay, nextStepHandler, currentStep } = useContext(AuthContext);
   console.log(path, currentStep);
 
   let formattedDate = null;
@@ -50,61 +39,19 @@ function DateSelectionStep({
     console.log(selectedCourtType);
   };
 
-  const courtTypeImages = {
-    "Clay Courts": "/images/clay.jpg",
-    "Hard Courts": "/images/hard.jpg",
-  };
-
-  // const handleShowCourts = () => {
-  //   setIsShowCourts(!isShowCourts);
-  // };
-
-  // const decreasePlayers = () => {
-  //   if (numberOfPlayers > 1) {
-  //     setNumberOfPlayers(numberOfPlayers - 1);
-  //   }
-  // };
-
-  // const increasePlayers = () => {
-  //   if (numberOfPlayers < 4) {
-  //     setNumberOfPlayers(numberOfPlayers + 1);
-  //   }
-  // };
-
-  // function dateSelectionHandler() {
-  // console.log("clicked");
-  // console.log(activeDay);
-  // console.log(router.route);
-  // const encodedDay = encodeURIComponent(activeDay);
-  // nextStepHandler();
-  // router.push(`/booking/?date=${formattedDate}`);
-  // }
-
-  // const playersAndCourtSelectionStyle = {
-  //   backgroundImage: `url(${courtTypeImages[selectedCourtType]})`,
-  //   backgroundPosition: "center",
-  //   backgroundRepeat: "no-repeat",
-  //   backgroundSize: "cover",
-  //   width: "500px",
-  //   height: "400px",
-  //   filter: "brightness(0.7)",
-  // };
-
   return (
     <Fragment>
       <div className={classes.firstStepContainer}>
         <div className={classes.playersAndCourtContainer}>
           <div>
-            <h1>Choose Court & Players Number:</h1>
+            <h1>Choose Court & Players:</h1>
           </div>
-          <div>
-            <label htmlFor="courtType">Court Type:</label>
+          <div className={classes.playersAndCourt}>
             <select value={selectedCourtType} onChange={handleCourtTypeChange}>
               <option>--- Select Court Type ---</option>
               <option value="Clay Court">Clay Court</option>
               <option value="Hard Court">Hard Court</option>
             </select>
-            <label htmlFor="numPlayers">Number of Players:</label>
             <select>
               <option>--- Select Players Number ---</option>
               <option value="1">1</option>
@@ -114,7 +61,7 @@ function DateSelectionStep({
             </select>
           </div>
         </div>
-        <div className={classes.playersDateContainer}>
+        {/* <div className={classes.playersDateContainer}>
           {selectedCourtType === "Clay Court" ? (
             <Image
               src={clay}
@@ -132,51 +79,25 @@ function DateSelectionStep({
               priority={true}
             />
           )}
-          {/* <div
-            style={playersAndCourtSelectionStyle}
-            className={classes.playersAndCourtSelectionStyle}
-          >
-            <div className={classes.courtsContainer}>
-              <h3>Courts:</h3>
-              <AiFillCaretDown
-                onClick={handleShowCourts}
-                style={{ marginTop: "1rem" }}
-              />
-              <div>
-                {isShowCourts && (
-                  <ul>
-                    <li onClick={handleChangeCourts}>Clay Courts</li>
-                    <li onClick={handleChangeCourts}>Hard Courts</li>
-                  </ul>
-                )}
-              </div>
-            </div>
-            <div>
-              <h3>Players:</h3>
-              <span>{numberOfPlayers}</span>
-              <button onClick={increasePlayers}>+</button>
-              <button onClick={decreasePlayers}>-</button>
-            </div>
-          </div> */}
+
           <div>
             <BookingCalendar nextStepHandler={nextStepHandler} />
           </div>
-        </div>
+        </div> */}
         <div className={classes.buttonContainer}>
           {activeDay ? (
-            <Link href="/booking" style={{ color: "white" }}>
-              <div
-                // whileHover={{ scale: 1.1 }}
-                // whileTap={{ scale: 0.9 }}
-                href={`/booking/?date=${formattedDate}`}
-                onClick={() => nextStepHandler()}
-                className={classes.nextButton}
-              >
-                Next
-              </div>
+            <Link
+              href={`/booking/?date=${formattedDate}`}
+              className={classes.nextButton}
+              style={{ color: "white" }}
+              onClick={() => nextStepHandler()}
+            >
+              Next <RightArrow />
             </Link>
           ) : (
-            <div className={classes.nextButtonDisabled}>Next</div>
+            <div className={classes.nextButtonDisabled}>
+              Next <RightArrow />
+            </div>
           )}
         </div>
       </div>
@@ -185,11 +106,3 @@ function DateSelectionStep({
 }
 
 export default DateSelectionStep;
-
-{
-  /* <div className={classes.bookingPlayers}>
-  
-
- 
-</div>; */
-}
