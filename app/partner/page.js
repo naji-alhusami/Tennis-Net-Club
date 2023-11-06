91a2461299ebc4fb34a82f9f9e7fe94dd2ce16d6
naji-alhusami <najihussami@gmail.com>
2023-11-06 16:48:20 +0300

    added partner page

--- app/partner/page.js
+++ app/partner/page.js
@@ -1,11 +1,11 @@
+import FindPartner from "@/components/find-partner/find-partner";
 import { fetchPlayersFromMongo } from "@/lib/players/fetchPlayersFromMongo";
 import React from "react";
 
 async function PartnerPage() {
   const players = await fetchPlayersFromMongo();
-  console.log(players);
-  //   return <FindPartner />;
-  return <h1>test</h1>;
+
+  return <FindPartner players={players.data} />;
 }
 
 export default PartnerPage;
