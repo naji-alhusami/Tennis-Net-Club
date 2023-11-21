4ef29230d8e04c0792ef3890eacd005c6f61a6ca
naji-alhusami <najihussami@gmail.com>
2023-11-21 13:20:13 +0300

    delete html file

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import MainNavigation from "@/components/layout/main-navigation";
 import "./globals.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import Footer from "@/components/layout/footer";
 import MyProvider from "@/store/my-provider";
 import { AuthProvider } from "./Providers";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
