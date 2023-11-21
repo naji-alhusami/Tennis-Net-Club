d570dd86c37474a0dc1293082e702e0e6f0e3f6b
naji-alhusami <najihussami@gmail.com>
2023-11-21 15:11:40 +0300

    added thanks page

---
+++ app/thanks/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Thanks from "@/components/thanks/thanks";
+
+function ThanksPage({ searchParams }) {
+  const thanksMessage = searchParams.thanks;
+  return <Thanks thanksMessage={thanksMessage} />;
+}
+
+export default ThanksPage;
