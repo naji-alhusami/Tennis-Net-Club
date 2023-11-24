6e90c3b2f525bbc6740a7699e6bd43e9bbb7ddb7
naji-alhusami <najihussami@gmail.com>
2023-11-24 23:46:49 +0300

    another edit on signup action

--- actions/SignupAction.js
+++ actions/SignupAction.js
@@ -16,51 +16,49 @@ console.log("baseURL in signupAction:", BASE_URL);
 // Signup With Credentials
 export async function signupWithCredentials(data) {
   try {
-    const { name, email, password, passwordConfirmation, number, level } = data;
-
-    const user = await User.findOne({ email: email });
+    const user = await User.findOne({ email: data.email });
     if (user) {
       return {
         error: "Email already Exists!",
       };
     }
 
-    if (!name || name.trim().length > 10) {
+    if (!data.name || data.name.trim().length > 10) {
       return {
         error: "Name Should Not Be More than 10 Characters",
       };
     }
 
-    if (!password || password.trim().length < 6) {
+    if (!data.password || data.password.trim().length < 6) {
       return {
         error: "Password Should Be More than 6 Characters",
       };
     }
 
-    if (password !== passwordConfirmation) {
+    if (data.password !== data.passwordConfirmation) {
       return {
         error: "Password Should Be More than 6 Characters",
       };
     }
 
-    if (password) {
-      password = await hashPassword(password);
+    if (data.password) {
+      data.password = await hashPassword(data.password);
     }
 
     // Create Token for Email Verification:
     const token = generateToken({ user: data });
 
     const newUser = new User({
-      name,
-      email,
-      WhatsAppNumber: number,
-      password,
-      level,
+      name: data.name,
+      email: data.email,
+      WhatsAppNumber: data.number,
+      password: data.password,
+      level: data.level,
       emailVerified: false,
     });
 
     await sendEmail({
-      to: email,
+      to: data.email,
       url: `${BASE_URL}/verify?token=${token}`,
       text: "VERIFY EMAIL",
     });
