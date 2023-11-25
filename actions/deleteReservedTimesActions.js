2a92e1459dacfc8ae581717970ca2073e57e953d
naji-alhusami <najihussami@gmail.com>
2023-11-25 14:38:01 +0300

    added manage reservations page

--- actions/deleteReservedTimesActions.js
+++ actions/deleteReservedTimesActions.js
@@ -1,19 +1,19 @@
-// "use server";
-// import { revalidatePath } from "next/cache";
+"use server";
+import { revalidatePath } from "next/cache";
 
-// import TakenTime from "@/models/takenTimeModel";
+import TakenTime from "@/models/takenTimeModel";
 
-// export async function deleteReservedTimesActions(timeSlot) {
-//   try {
-//     const result = await TakenTime.deleteOne({ _id: timeSlot._id });
-    
-//     if (result.deletedCount === 1) {
-//       revalidatePath("/manage");
-//       return { message: "Deleted Reserved Time Successfully" };
-//     } else {
-//       throw new Error("No document with this id was found.");
-//     }
-//   } catch (error) {
-//     throw new Error("An error occurred while deleting the reserved time.");
-//   }
-// }
+export async function deleteReservedTimesActions(timeSlot) {
+  try {
+    const result = await TakenTime.deleteOne({ _id: timeSlot._id });
+
+    if (result.deletedCount === 1) {
+      revalidatePath("/manage");
+      return { message: "Deleted Reserved Time Successfully" };
+    } else {
+      throw new Error("No document with this id was found.");
+    }
+  } catch (error) {
+    throw new Error("An error occurred while deleting the reserved time.");
+  }
+}
---
+++ components/manage/manage-reservations.js
@@ -0,0 +1,30 @@
+"use server";
+import React, { Fragment } from "react";
+import { getServerSession } from "next-auth";
+
+import ManageBookingForm from "./manage-reservation-form";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { fetchTakenTimesFromMongo } from "@/lib/takenTimes/fetchTakenTimesFromMongo";
+
+async function ManageReservations({ cancelReservedTimeHandler }) {
+  const session = await getServerSession(authOptions);
+  const takenTimes = await fetchTakenTimesFromMongo();
+  const filteredTakenTimes = takenTimes.data.filter(
+    (takenTime) => takenTime.member === session?.user.name
+  );
+
+  return (
+    <Fragment>
+      {!filteredTakenTimes.length > 0 ? (
+        <p>You Do Not Have Reserved Times</p>
+      ) : (
+        <ManageBookingForm
+          cancelReservedTimeHandler={cancelReservedTimeHandler}
+          filteredTakenTimes={filteredTakenTimes}
+        />
+      )}
+    </Fragment>
+  );
+}
+
+export default ManageReservations;
